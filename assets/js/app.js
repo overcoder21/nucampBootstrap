dbe9b9618fbc39707fed827aca94f96a56e81eaf
Greg Jabonete <gregjabonete2@outlook.com>
2021-07-24 14:08:53 -0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+feather.replace()
+
+$('#myModal').modal('show')
+
+
+@section Scripts {
+    <script>
+        $(function () {
+            $('btnSave').on('click', function (event) {
+                alert();
+                event.preventDefault();
+            });
+
+        });
+
+    </script>
+}
